b35816ae75845fb895fb2483079f15c549973f8b
codygman <cody@codygman.dev>
2023-02-06 15:46:54 -0600

    initial commit

---
+++ roam/.dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((org-roam-directory . "/home/cody/org-roam/data/d8/799397-a065-4c51-947b-0e30b534440b/roam")
+         (org-roam-db-location . "/home/cody/org-roam/data/d8/799397-a065-4c51-947b-0e30b534440b/roam/org-roam.db"))))
